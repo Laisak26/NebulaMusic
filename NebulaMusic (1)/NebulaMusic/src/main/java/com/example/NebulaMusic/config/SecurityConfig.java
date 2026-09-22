1aead6aa2796bb75614551f8a54033331587a752
Laisak26 <isaacgerhardus@gmail.com>
2026-09-21 21:49:50 -0700

    Initial commit

---
+++ NebulaMusic (1)/NebulaMusic/src/main/java/com/example/NebulaMusic/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.NebulaMusic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/index", "/index.html",
+                                "/iniciar-sesion", "/iniciar-sesion.html",
+                                "/registro", "/registro.html", "/error",
+                                "/error.html").permitAll()
+
+                        .requestMatchers(
+                                "/crear-cuenta", "/autenticacion", "/cerrar-sesion").permitAll()
+
+                        .requestMatchers(
+                                "/static/**", "/styles/**", "/imgs/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
